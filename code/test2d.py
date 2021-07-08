61c55cffd3ec922d4744a199e36232595edc0202
segtran <segtran.github@gmail.com>
2021-07-08 17:25:27 +0800

    finer split of dataset

--- code/test2d.py
+++ code/test2d.py
@@ -43,7 +43,7 @@ parser.add_argument('--ds', dest='ds_name', type=str, default='valid2', help='Da
 parser.add_argument('--split', dest='ds_split', type=str, default='all',
                     choices=['train', 'test', 'all'], help='Split of the dataset')
 parser.add_argument('--samplenum', dest='sample_num', type=int,  default=-1, 
-                    help='Numbers of samples in the dataset split used to train the model (Default: -1, no images are used in the dataset). '
+                    help='Numbers of samples in the dataset split used to train the model (Default: -1, no images are used in the dataset).')
 parser.add_argument('--cpdir', dest='checkpoint_dir', type=str, default=None,
                     help='Load checkpoint(s) from this directory')
 parser.add_argument('--iters', type=str,  default='8000,7000', help='checkpoint iteration(s)')
