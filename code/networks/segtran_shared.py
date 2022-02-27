5bdffc9baad2ebd25243be6cfa5aa556373f08b6
askerlee <shaohua@gmail.com>
2022-02-27 19:50:12 +0800

    add attention consistency loss

--- code/networks/segtran_shared.py
+++ code/networks/segtran_shared.py
@@ -894,7 +894,7 @@ class SegtranFusionEncoder(nn.Module):
         self.comb_norm_layers   = nn.ModuleList(comb_norm_layers)
         self.vfeat_norm_layers  = nn.ModuleList(vfeat_norm_layers)
         # attention_scores: [B0, 4, N1, N2] => [B0, 1, N1, N2]
-        self.attn_scaler        = nn.Conv2d(config.num_modes, 1)
+        self.attn_scaler        = nn.Conv2d(config.num_modes, 1, 1)
 
     def forward(self, vfeat, voxels_pos, vmask, orig_feat_shape):
         self.layers_vfeat = []
